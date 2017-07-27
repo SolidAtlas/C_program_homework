81a0990b6e826250b1b60704f33daea715e2d05e
SolidAtlas <justin.d.garcia@gmail.com>
2017-07-27 14:30:11 -0700

    format

--- C programs/p2.c
+++ C programs/p2.c
@@ -11,15 +11,15 @@ int main (void)
 int first, last;
     double d;
     printf("enter two numbers and exponent\n");
-scanf("%i,%i,%lf", &first,&last, &d);
+    scanf("%i,%i,%lf", &first,&last, &d);
     powertable(first,last,d);
    
 
 return 0;
 }
 void powertable (int first, int last,double d){
-  for(;first<=last;first++){
+    for(;first<=last;first++){
         
-printf("%i \t %.2lf \n", first,pow(first,d));
+    printf("%i \t %.2lf \n", first,pow(first,d));
     }  
 }
